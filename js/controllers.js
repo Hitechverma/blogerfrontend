78523a67581287c978e810ca7e876883c38d50cd
hitechverma <hitechlov22@gmail.com>
2015-09-01 00:45:25 +0530

    Get Done

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,17 @@
 blogApp.controller('HomeController',function($scope, $http) {
 	$http.get(API_URL + "posts/").
 	success(function(data, status, headers, config){
-		$scope.data = data;
-		console.log($scope.data, "hiiii buddy")
+		$scope.t_posts = data;
+		console.log($scope.t_posts, "hiiii buddy")
+		angular.forEach($scope.t_posts,function(posts){
+			$scope.post = posts.post
+			$scope.username = posts.username
+			$scope.createdAt = posts.created_at
+
+			console.log('this is a post', $scope.post);
+			console.log('this is that user', $scope.username);
+			console.log('this was the time\n', $scope.createdAt);
+
+		})
 	})
 })
