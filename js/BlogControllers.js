66bbda4063156573eedbd1c12556c5d77bea4c30
hitechverma <hitechlov22@gmail.com>
2016-04-02 19:18:49 +0530

    new

--- js/BlogControllers.js
+++ js/BlogControllers.js
@@ -1,4 +1,11 @@
-blogApp.controller('BlogCtrl',['$scope','$http',function($scope,$http){
+blogApp.controller('BlogCtrl',['$scope','$http','$stateParams',function($scope,$http,stateParams){
 	
+	var blog_id = stateParams.idx
+	console.log(blog_id)
+	$http.get(API_URL + "blogs/" + blog_id).
+		success(function(data,status,header,config){
+			$scope.blog_post = data
+			console.log(data)
+		})
 	
 }])
